3d902e5ee8d2dbab09b1a7b0b4afdddb5fb5410f
Michael Standen <screaminghawk@gmail.com>
2021-04-10 08:35:04 +1200

    Clear outputs clears token_data folder too

--- clearoutputs.py
+++ clearoutputs.py
@@ -1,5 +1,6 @@
 import shutil
 from glob import glob
 
-# Deletes all folders inside the "output" folder
+# Deletes all folders inside the "output" and "token_data" folders
 [shutil.rmtree(f) for f in glob("output/*/")]
+[shutil.rmtree(f) for f in glob("token_data/")]
